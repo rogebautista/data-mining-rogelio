1eb0777f75f689415e4ddeecb2bb719c5ca075d6
roge2401 <3jW1UTcojrs0>
2022-12-18 16:06:38 -0600

    limpieza de notebook

--- main.R
+++
@@ -1,6 +0,0 @@
-airbnb<-read.csv('airbnb-listings.csv',sep = ';')
-
-options(repr.plot.height=4,repr.plot.width=6,repr.plot.res = 300)
-
-airbnb_reduced <- airbnb[,c('City','Room.Type','Neighbourhood','Accommodates','Bathrooms','Bedrooms','Beds','Price','Square.Feet','Guests.Included','Extra.People','Review.Scores.Rating','Latitude', 'Longitude')]
-airbnb_reduced
